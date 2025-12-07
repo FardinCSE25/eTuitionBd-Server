db953ce08f93537ca106de28d2effbb412480a1b
Fardin Ahmed <fardin.cse25@gmail.com>
2025-12-07 17:18:13 +0600

    origin added in cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://etuitionbd-a1c8c.web.app"],
     methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
     credentials: true,
   })
